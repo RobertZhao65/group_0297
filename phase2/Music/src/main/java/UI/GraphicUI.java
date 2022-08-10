3a77e3ac5b27aaab61ac0ab7f79a1664dad5874d
Boa Yang <boa.yang@mail.utoronto.ca>
2022-08-09 21:47:05 -0400

    GUI-display login menu to the users.

--- phase2/Music/src/main/java/UI/GraphicUI.java
+++ phase2/Music/src/main/java/UI/GraphicUI.java
@@ -1,19 +1,55 @@
 package UI;
-
 import MusicUtil.Album;
 import MusicUtil.Song;
 
 import javax.swing.*;
 import java.util.List;
 
-public class GraphicUI implements UIMethods {
-    private JFrame frame;
+public class GraphicUI implements UIMethods{
+    private static JLabel userLabel;
+    private static JTextField username;
+    private static JLabel passwordLabel;
+    private static JTextField password;
+    private static JButton login;
+    private static JLabel success;
 
-    public void displayWindow() {
-        frame.setSize(1920, 1080);
-        frame.setLayout(null);
+    public static void main(String[] args){
+        JPanel panel = new JPanel();
+        JFrame frame = new JFrame();
+        frame.setTitle("Login to Music Player");
+        frame.setSize(400, 350);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        userLabel = new JLabel("Username");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        username = new JTextField(15);
+        username.setBounds(100, 20, 180,25);
+        panel.add(username);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50,80, 25);
+        panel.add(passwordLabel);
+
+        password = new JPasswordField();
+        password.setBounds(100, 50, 180, 25);
+        panel.add(password);
+
+        login = new JButton("Login");
+        login.setBounds(115,80,150, 25);
+
+        panel.add(login);
+
+        JButton create = new JButton("Create Account");
+        create.setBounds(115, 110, 150, 25);
+        panel.add(create);
         frame.setVisible(true);
     }
+
 
     public void displayLoginMenu() {
 
@@ -27,7 +63,6 @@ public class GraphicUI implements UIMethods {
      * Display the message which indicates that the user's login attempt was successful
      */
     public void loginSuccess() {
-
     }
 
     /**
@@ -96,6 +131,6 @@ public class GraphicUI implements UIMethods {
 
     @Override
     public boolean displayAlbums(List<Album> albums) {
-        return true;
+        return false;
     }
 }
